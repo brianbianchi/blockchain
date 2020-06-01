53402d41cabe21839b56306217afa40c45a32788
brianbianchi <bianchibrianj@gmail.com>
2020-06-01 18:06:12 -0400

    give hash to genesis

--- src/block.py
+++ src/block.py
@@ -13,7 +13,7 @@ class Block:
 
     def to_json(self):
         """
-        Converts Block object to JSON object
+        Converts Block to JSON object
 
         :return: dict (JSON object)
         """
@@ -26,7 +26,7 @@ class Block:
 
         :return: Block
         """
-        return Block(0, time(), [], 100, '1')
+        return Block(0, time(), [], 100, '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b')
 
     @staticmethod
     def hash(block):
